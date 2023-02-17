e19a5982cde110e59345c60d88df7287ab96b041
munsoyeon <munsy0111@gmail.com>
2023-02-17 14:04:09 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,8 +1,67 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a
+          href="/"
+          onClick={function (event) {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  const list = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let topic = props.topics[i];
+    list.push(
+      <li key={topic.id}>
+        <a href={"/read/" + topic.id}>{topic.title}</a>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>{list}</ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  const topics = [
+    { id: 1, title: "html", body: "html is..." },
+    { id: 2, title: "css", body: "css is..." },
+    { id: 3, title: "js", body: "js is..." },
+  ];
+  return (
+    <div>
+      <Header
+        title="WEB"
+        onChangeMode={function () {
+          alert("Header");
+        }}
+      />
+      <Nav topics={topics} />
+      <Article title="Welcome" body="Hello, WEB" />
+    </div>
+  );
 }
 
 export default App;
